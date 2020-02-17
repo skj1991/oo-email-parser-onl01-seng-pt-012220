897a6eccad2cd828b2c4996a23e5cecb9e25e96a
Stacy Johns <stacykjohns@icloud.com>
2020-02-17 21:52:44 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailAddressParser
   end
 
   def parse
-    email_addresses.split(/[, ]/).uniq.delete_if{|e| e.length == 0}
+    email_addresses.split(/[, ]/).uniq.delete_if{|email| email.length == 0}
   end
 
 end
